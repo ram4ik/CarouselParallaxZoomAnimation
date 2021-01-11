60c050e6adc8e19e8ef5653519ce39ee82424484
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-01-11 19:16:22 +0200

    Initial Commit

---
+++ CarouselParallaxZoomAnimation/CarouselParallaxZoomAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselParallaxZoomAnimationApp.swift
+//  CarouselParallaxZoomAnimation
+//
+//  Created by Ramill Ibragimov on 11.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct CarouselParallaxZoomAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
